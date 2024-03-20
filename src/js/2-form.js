9cc327cecc1b58ee85eb5236866e3d7d508c919c
OleksijHr <hrymovskij@gmail.com>
2024-03-20 23:10:18 +0200

    hw-09.3

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,65 +1,47 @@
-// Виконуй це завдання у файлах 2-form.html і 2-form.js. Розбий його на декілька підзавдань:
-
-// Використовуючи делегуваня, відстежуй на формі подію input і щоразу записуй у локальне сховище об'єкт з полями email і message, 
-// у яких зберігай поточні значення полів форми.Нехай ключем для сховища буде рядок "feedback-form-state".
-// Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, то заповнюй ними поля форми. 
-// В іншому випадку поля повинні бути порожніми.
-// Під час сабміту форми очищай сховище і поля форми, а також виводь у консоль об'єкт з полями email,
-//  message та їхніми поточними значеннями.
-
-
-
-const form = document.querySelector(".feedback-form");
-const input = document.querySelector("[type='email']");
-const submitBtn = document.querySelector("[type='submit']");
-const email = form.elements.email;
-const textarea = form.elements.message;
 const localStorageKey = "feedback-form-state";
 
-// const user = {
-//     email: "",
-//     message: "",
-// }
+const form = document.querySelector(".feedback-form");
+const emailInput = document.querySelector("[type='email'");
+const feedbackMessage = document.querySelector("textarea");
 
-email.value = localStorage.getItem(localStorageKey) ?? "";
-textarea.value = localStorage.getItem(localStorageKey) ?? "";
 
-form.addEventListener("input", handlerInput);
-form.addEventListener("submit", handlerSubmit);
 
-function handlerInput(event) {
-    localStorage.setItem(localStorageKey, event.target.value);
-
+const user = {
+    email: "",
+    message: "",
 };
 
-function handlerSubmit(event) {
+form.addEventListener("input", onTextareaInput);
+form.addEventListener("submit", handleSubmit);
+
+populateTextarea();
+
+
+function handleSubmit(event) {
     event.preventDefault();
-    console.log(event.target.elements.email.value);
-    console.log(event.target.elements.message.value);
+
+    console.log(user);
 
     localStorage.removeItem(localStorageKey);
-
-    form.requestFullscreen();
-};
-
-
-    // const email = event.currentTarget.elements.email;
-    // const message = event.currentTarget.elements.message;
-
-    // if (email.value === '' && message.value === '') {
-    //     alert("Будь ласка заповніть усі обов'язкові поля")
-    // } else {
-    //     const feedback = {};
-
-    //     feedback.email = email.value.trim();
-    //     feedback.message = message.value.trim();
-
-    //     console.log(feedback);
-
-        // event.currentTarget.reset();
-    // }
-// } 
-
-console.log(localStorage);
+    event.target.reset();
+}
 
 
+function populateTextarea(event) {
+    const savedEmail = localStorage.getItem(localStorageKey, user.email);
+    const savedMessage = localStorage.getItem(localStorageKey, user.message);
+
+    if (savedEmail && savedMessage) {
+        emailInput.value = JSON.parse(savedEmail).email;
+        feedbackMessage.value = JSON.parse(savedMessage).message;
+    }
+}
+
+
+
+function onTextareaInput(event) {
+    user.email = event.currentTarget.elements.email.value;
+    user.message = event.currentTarget.elements.message.value;
+
+    localStorage.setItem(localStorageKey, JSON.stringify(user));
+}
